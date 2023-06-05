dbe1c0b7bd31f4e69ab6c734e3e98eecd1631eb2
roamingterra <danielballerini1@gmail.com>
2023-06-05 16:15:39 -0400

    Layout some pseudo-code and notes for the project
    I laid out some pseudo-code and notes for the test file and the app
    file. These notes describe the types of tests that I will setup prior to
    the building of the app, and the layout of the app (different pages,
    components, functionality, etc.)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  // STRUCTURE OF THE APP
+
+  // Router: 1. Home page 2. Shopping page
+
+  // Home page (component): Contains only styling, information, assets, and a shopping button that
+  // to the shopping page. The shopping button will be the only functional button navigates the user
+
+  // Shopping page (component): Contains styling, various products (components), and a sticky bar
+  // that contains the shopping cart. Each product component will be effectively the same component,
+  // but with different props being sent back and forth for the different product images, titles,
+  // descriptions, and quantities selected. Each product component will render a unique image, a product
+  // title, a product description, a quantity field, quantity buttons, and an add to cart button.
+  // The state of each product shall be managed by the shopping cart component, the product information
+  // and the necessary functions to handle the product logic will be passed down as props.
+
+  // The look of the app will take inspiration from the following websites:
+
+  // musixmatch for the home page and the shopping page
+  // https://about.musixmatch.com/pro?utm_source=musixmatch_website&utm_campaign=pro_announcement&utm_content=header_nav
+
+  // allbirds for the sticky bar and shopping cart
+  // https://www.allbirds.com/collections/best-for-traveling-mens
+
   return (
     <div className="App">
       <header className="App-header">
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,28 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import App from "./App";
 
-test('renders learn react link', () => {
+// Out of the box test that will eventually be deleted
+test("renders learn react link", () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+// Test the rendering of the home page
+
+// Test the routing behavior from the home page to the shopping page
+
+// Test the rendering of the shopping page
+
+// Test the functionality of the input fields for the user to manually type in the item quantities
+
+// Test the functionality of the increment and decrement buttons for the item quantities
+
+// Test the functionality of the add to cart buttons
+
+// Test adding multiple items to the shopping cart and see if the cart correctly displays the total...
+// quantity and the total price
+
+// Perform accessibility tests to ensure the components are accessible and responsive across various devices
