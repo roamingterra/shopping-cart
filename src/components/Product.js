db60579c31e939e95b691e2a0969e7b52f5d0b2c
roamingterra <danielballerini1@gmail.com>
2023-06-12 15:04:42 -0400

    Complete section 2 of shopping page
    The products are now listed and styled on the shopping page. Each
    product lists its title, a product image, price description, and an add
    to cart button. This section is also responsive across all devices.
    Finally, I switched images with regards to the arabic language software
    product.

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,7 +1,29 @@
 import React from "react";
+import shoppingCss from "./../styles/shopping.module.css";
 
-function Product() {
-  return <div></div>;
+function Product(props) {
+  const { productName, image, description, price } = props;
+
+  return (
+    <div className={shoppingCss["product"]}>
+      <h1>{productName}</h1>
+      <img
+        className={shoppingCss["product-image"]}
+        src={image}
+        alt="product"
+      ></img>
+      <div className={shoppingCss["product-price"]}>
+        <div>{`$${price}`}</div>
+        <div>one time payment</div>
+      </div>
+      <button className={shoppingCss["add-to-cart"]}>ADD TO CART</button>
+      <ul className={shoppingCss["product-descriptions"]}>
+        <li className={shoppingCss["product-description"]}>{description[0]}</li>
+        <li className={shoppingCss["product-description"]}>{description[1]}</li>
+        <li className={shoppingCss["product-description"]}>{description[2]}</li>
+      </ul>
+    </div>
+  );
 }
 
 export default Product;
--- src/components/Shopping.js
+++ src/components/Shopping.js
@@ -6,6 +6,18 @@ import shoppingCss from "./../styles/shopping.module.css";
 
 function Shopping() {
   const productInfo = {
+    productNames: {
+      english: "English",
+      french: "French",
+      mandarin: "Mandarin",
+      spanish: "Spanish",
+      german: "German",
+      japanese: "Japanese",
+      italian: "Italian",
+      portuguese: "Portuguese",
+      hindi: "Hindi",
+      arabic: "Arabic",
+    },
     imagePaths: {
       english: "/images/united-states-flag.png",
       french: "/images/france-flag.png",
@@ -14,73 +26,73 @@ function Shopping() {
       german: "/images/germany-flag.png",
       japanese: "/images/japan-flag.png",
       italian: "/images/italy-flag.png",
-      portuguese: "images/brazil-flag.png",
-      hindi: "images/india-flag.png",
-      arabic: "images/egypt-flag.png",
+      portuguese: "/images/brazil-flag.webp",
+      hindi: "/images/india-flag.png",
+      arabic: "/images/lebanon-flag.png",
     },
     descriptions: {
       english: [
-        "Widely spoken worldwide, mastering English opens doors to global opportunities.",
-        "Discover a language that empowers effective communication in business, travel, and academia.",
-        "Immerse yourself in English literature and entertainment, unlocking a world of captivating stories.",
+        "Widely spoken worldwide",
+        "Language of business, travel, and academia",
+        "Immerse yourself in English literature and entertainment",
       ],
       french: [
-        "Dive into the language of love and sophistication, exploring the rich cultural heritage of France.",
-        "Experience the elegance of French cuisine, fashion, and arts while conversing en français.",
-        "Enhance your résumé and expand your career prospects with fluency in the language of diplomacy.",
+        "Dive into the language of love and sophistication",
+        "Experience the elegance of French cuisine, fashion, and arts",
+        "Expand your career prospects with fluency in the language of diplomacy",
       ],
       mandarin: [
-        "Embrace the future by mastering the most spoken language in the world, connecting over a billion people.",
-        "Unlock access to China's vibrant culture, profound history, and booming business opportunities.",
-        "Challenge yourself with fascinating characters and tones, and gain an edge in an increasingly interconnected world.",
+        "Spoken by over a billion people",
+        "Vibrant culture, profound history, and booming business opportunities",
+        "Gain an edge in an increasingly interconnected world",
       ],
       spanish: [
-        "Immerse yourself in the passion and vibrancy of the Spanish-speaking world, from flamenco to fiestas.",
-        "Explore diverse cultures across Spain and Latin America, savoring the flavors of their cuisine.",
-        "Expand your horizons and connect with millions of Spanish speakers, both personally and professionally.",
+        "Immerse yourself in the passion and vibrancy of the Spanish-speaking world",
+        "Explore diverse cultures across Spain and Latin America",
+        "Expand your horizons and connect with millions of Spanish speakers",
       ],
       german: [
-        "Discover the language of precision, engineering, and innovation, spoken in the heart of Europe.",
-        "Immerse yourself in Germany's rich cultural heritage, from classical music to cutting-edge technology.",
-        "Open doors to career opportunities in industries such as automotive, technology, and renewable energy.",
+        "Discover the language of precision, engineering, and innovation",
+        "Immerse yourself in Germany's rich cultural heritage",
+        "Career opportunities in industries such as automotive and technology",
       ],
       japanese: [
-        "Embark on a journey through the fascinating world of Japan, blending tradition with technological prowess.",
-        "Dive into the captivating realm of anime, manga, and video games, experiencing Japanese pop culture.",
-        "Gain a competitive edge in business and forge strong connections with one of Asia's economic powerhouses.",
+        "Japan Blends tradition with technological prowess",
+        "Dive into Japanese pop culture",
+        "One of Asia's economic powerhouses",
       ],
       italian: [
-        "Indulge in the beauty of the Italian language, the language of art, music, and exquisite cuisine.",
-        "Discover the timeless charm of Italy's historic cities, breathtaking landscapes, and world-renowned fashion.",
-        "Learn Italian to truly immerse yourself in la dolce vita, unlocking the treasures of Italy's rich cultural heritage.",
+        "Indulge in the language of art, music, and exquisite cuisine",
+        "Discover Italy's historic cities, breathtaking landscapes, and renowned fashion",
+        "Unlock the treasures of Italy's rich cultural heritage",
       ],
       portuguese: [
-        "Immerse yourself in the vibrant rhythms of Portuguese, the language of Brazil and Portugal.",
-        "Explore the stunning beaches, diverse ecosystems, and vibrant festivals of Portuguese-speaking countries.",
-        "Enhance your global perspective and connect with millions of Portuguese speakers across continents.",
+        "The language of Brazil and Portugal",
+        "Explore diverse ecosystems and vibrant festivals of Portuguese-speaking countries",
+        "Connect with millions of Portuguese speakers across continents",
       ],
       hindi: [
-        "Dive into the colorful world of Bollywood, music, and spirituality through the Hindi language.",
-        "Explore the diverse cultures and traditions of India, the world's largest democracy.",
-        "Connect with a billion Hindi speakers worldwide and unlock opportunities in business, travel, and cultural exchange.",
+        "Dive into the colorful world of Bollywood, music, and spirituality",
+        "Explore the diverse cultures and traditions of India",
+        "Connect with a billion Hindi speakers worldwide",
       ],
       arabic: [
-        "Embark on a journey to the cradle of civilization, unlocking the beauty and rich history of the Arabic language.",
-        "Discover the poetic nature of Arabic script, and delve into the wisdom of ancient philosophical texts.",
-        "Gain a competitive edge in diplomacy, business, and international relations in the Middle East and North Africa.",
+        "Embark on a journey to the cradle of civilization",
+        "Discover the poetic nature of Arabic script",
+        "Gain a competitive edge in diplomacy and business in the Middle East and North Africa",
       ],
     },
     pricing: {
-      english: 99,
-      french: 79,
-      mandarin: 109,
-      spanish: 89,
-      german: 79,
-      japanese: 99,
-      italian: 79,
-      portuguese: 79,
-      hindi: 89,
-      arabic: 99,
+      english: 99.99,
+      french: 79.99,
+      mandarin: 109.99,
+      spanish: 89.99,
+      german: 79.99,
+      japanese: 99.99,
+      italian: 79.99,
+      portuguese: 79.99,
+      hindi: 89.99,
+      arabic: 99.99,
     },
   };
 
@@ -114,51 +126,61 @@ function Shopping() {
         </div>
         <div className={shoppingCss["main-content-2"]}>
           <Product
+            productName={productInfo.productNames.english}
             image={productInfo.imagePaths.english}
             description={productInfo.descriptions.english}
             price={productInfo.pricing.english}
           ></Product>
           <Product
+            productName={productInfo.productNames.french}
             image={productInfo.imagePaths.french}
             description={productInfo.descriptions.french}
             price={productInfo.pricing.french}
           ></Product>
           <Product
+            productName={productInfo.productNames.mandarin}
             image={productInfo.imagePaths.mandarin}
             description={productInfo.descriptions.mandarin}
             price={productInfo.pricing.mandarin}
           ></Product>
           <Product
+            productName={productInfo.productNames.spanish}
             image={productInfo.imagePaths.spanish}
             description={productInfo.descriptions.spanish}
             price={productInfo.pricing.spanish}
           ></Product>
           <Product
+            productName={productInfo.productNames.german}
             image={productInfo.imagePaths.german}
             description={productInfo.descriptions.german}
             price={productInfo.pricing.german}
           ></Product>
           <Product
+            productName={productInfo.productNames.japanese}
             image={productInfo.imagePaths.japanese}
             description={productInfo.descriptions.japanese}
             price={productInfo.pricing.japanese}
           ></Product>
           <Product
+            productName={productInfo.productNames.italian}
             image={productInfo.imagePaths.italian}
             description={productInfo.descriptions.italian}
             price={productInfo.pricing.italian}
           ></Product>
           <Product
+            productName={productInfo.productNames.portuguese}
             image={productInfo.imagePaths.portuguese}
             description={productInfo.descriptions.portuguese}
             price={productInfo.pricing.portuguese}
           ></Product>
           <Product
+            productName={productInfo.productNames.hindi}
             image={productInfo.imagePaths.hindi}
             description={productInfo.descriptions.hindi}
             price={productInfo.pricing.hindi}
           ></Product>
           <Product
+            productName={productInfo.productNames.arabic}
             image={productInfo.imagePaths.arabic}
             description={productInfo.descriptions.arabic}
             price={productInfo.pricing.arabic}
